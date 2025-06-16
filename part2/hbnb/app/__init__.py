90566736c2dfa8bf2499f3548ea2883dcedde703
User10538 <10538@holbertonstudents.com>
2025-06-17 00:28:15 +1000

    Add namespace for places and amenities

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.places import api as places_ns
 
 def create_app():
     app = Flask(__name__)
@@ -12,4 +14,10 @@ def create_app():
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
 
+    # Register the amenities namespace
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+
+    # Register the places namespace
+    api.add_namespace(places_ns, path='/api/v1/places')
+    
     return app
--- part2/hbnb/app/api/v1/amenities.py
+++ part2/hbnb/app/api/v1/amenities.py
@@ -1,5 +1,5 @@
 from flask_restx import Namespace, Resource, fields
-from flake import request
+from flask import request
 from app.services import facade
 
 api = Namespace('amenities', description='Amenity operations')
