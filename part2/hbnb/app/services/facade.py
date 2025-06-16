a0cf654cddbcf8b31fcf5889e1837732e6e66e04
User10538 <10538@holbertonstudents.com>
2025-06-16 10:22:23 +1000

    Update facade.py methods

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -16,3 +16,18 @@ class HBnBFacade:
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
+    def create_amenity(self, amenity_data):
+        # Placeholder for logic to create an amenity
+        pass
+
+    def get_amenity(self, amenity_id):
+        # Placeholder for logic to retrieve an amenity by ID
+        pass
+
+    def get_all_amenities(self):
+        # Placeholder for logic to retrieve all amenities
+        pass
+
+    def update_amenity(self, amenity_id, amenity_data):
+        # Placeholder for logic to update an amenity
+        pass
