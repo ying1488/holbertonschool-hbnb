cb987f96e0da32c6bc1000f78e728f253c1a2d95
User10538 <10538@holbertonstudents.com>
2025-06-16 14:45:52 +1000

    Save files

--- part2/hbnb/app/api/v1/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask_restx import Api
-from .v1.amenities import api as amenities_ns
-
-api = Api()
-
-def initialize_app(app):
-    api.init_app(app)
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
--- part2/hbnb/app/persistence/repository.py
+++ part2/hbnb/app/persistence/repository.py
@@ -58,4 +58,3 @@ class InMemoryRepository(Repository):
 
     def get_by_attribute(self, attr_name, attr_value):
         return next((obj for obj in self._storage.values() if getattr(obj, attr_name) == attr_value), None)
-    
