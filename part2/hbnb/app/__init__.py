108cc70e3847281c6d7d3591f711f90a8aba3ca4
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-17 05:39:10 +0000

    added namespace for review

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -3,6 +3,7 @@ from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
+from app.api.v1.reviews import api as review_ns
 
 def create_app():
     app = Flask(__name__)
@@ -20,4 +21,7 @@ def create_app():
     # Register the places namespace
     api.add_namespace(places_ns, path='/api/v1/places')
     
+    # Register the places namespace
+    api.add_namespace(review_ns, path='/api/v1/reviews')
+
     return app
