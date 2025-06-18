d9866f5ec50eb23059926915234c829df3dd2915
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-18 05:57:11 +0000

    updated place model to include reviews

--- part2/hbnb/app/api/v1/places.py
+++ part2/hbnb/app/api/v1/places.py
@@ -32,7 +32,7 @@ place_model = api.model('Place', {
     'latitude': fields.Float(required=True, description='Latitude of the place'),
     'longitude': fields.Float(required=True, description='Longitude of the place'),
     'owner_id': fields.String(required=True, description='ID of the owner'),
-    'amenities': fields.List(fields.String, required=True, description="List of amenities ID's")
+    'amenities': fields.List(fields.String, required=True, description="List of amenities ID's"),
     'reviews': fields.List(fields.Nested(review_model), description='List of reviews')
 })
 
