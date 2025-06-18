06d1425e042456b83660485de2a0fa04aa957a07
User10538 <10538@holbertonstudents.com>
2025-06-18 10:17:53 +1000

    Fix error

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -15,6 +15,8 @@ class InvalidPlaceUpdateError(Exception):
 class HBnBFacade:
     def __init__(self):
                  
+        self.user_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
