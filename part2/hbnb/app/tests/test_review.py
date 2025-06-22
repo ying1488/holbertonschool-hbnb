e504f8383ff571b498db0f62aa6b4496e97745ac
ying1488 <yingintech@gmail.com  git config --global user.name Ying>
2025-06-22 21:11:28 +0000

    test to create a new review passed

--- part2/hbnb/app/tests/test_review.py
+++ part2/hbnb/app/tests/test_review.py
@@ -10,6 +10,9 @@ from app.models.user import User
 class TestReview(unittest.TestCase):
     """Test that the Review model works as expected
     """
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
 
     def test_create_review(self):
         """Test creation of Review instances"""
@@ -18,33 +21,37 @@ class TestReview(unittest.TestCase):
             "first_name": "Jane",
             "last_name": "Doe",
             "email": "jane.doe@example.com"})
-        self.user.id = json.loads(UserResponse.data)['id']
-
-        # Create test place
-        PlaceResponse = self.client.post('/api/v1/places/', json={
-            "title": "Review Test Place",
-            "price": 80.0,
-            "latitude": 34.0522,
-            "longitude": -118.2437,
-            "owner_id": self.user_id,
-            "amenities": []
-        })
+        print("Response status:", UserResponse.status_code)
+        print("Response data:", UserResponse.get_json())
+        self.assertEqual(UserResponse.status_code, 201)
+        self.user_id = json.loads(UserResponse.data)['id']
+        
+        # Create Place user
+        PlaceResponse = self.client.post('/api/v1/places/', 
+                                        json={
+                                            "title": "Review Test Place",
+                                            "description": "description",
+                                            "price": 80.0,
+                                            "latitude": 34.0522,
+                                            "longitude": -118.2437,
+                                            "owner_id": self.user_id,
+                                            "amenities": []
+                                        })
         self.place_id = json.loads(PlaceResponse.data)['id']
+        print("Response status:", PlaceResponse.status_code)
+        print("Response data:", PlaceResponse.get_json())
+        self.assertEqual(PlaceResponse.status_code, 201)
 
-    def test_create_review_success(self):
-        """Test successful review creation"""
-        res = self.client.post('/api/v1/reviews/', 
-                                  json={
-                                      "text": "Great place to stay!",
-                                      "rating": 5,
-                                      "user_id": self.user_id,
-                                      "place_id": self.place_id
-                                  })
-        self.assertEqual(res.status_code, 201)
-        data = json.loads(res.data)
-        self.assertIn('id', data)
-        self.assertEqual(data['test'], 'Great place to stay!')
-        self.assertEqual(data['rating'], 5)
-    
+         # Create Place user
+        review_response = self.client.post('/api/v1/reviews/', json={
+            "text": "Awesome stay!",
+            "rating": 5,
+            "user_id": self.user_id,
+            "place_id": self.place_id
+            })
+        self.assertEqual(review_response.status_code, 201)
+        self.assertIn("id", review_response.get_json())
+
+
 if __name__ == '__main__':
     unittest.main()
