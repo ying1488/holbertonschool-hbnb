4b788e254161abf6de33a68e3783a4131919d20f
User10538 <10538@holbertonstudents.com>
2025-07-05 20:45:43 +1000

    Add place model

--- part3/hbnb/app/models/place.py
+++ part3/hbnb/app/models/place.py
@@ -1,7 +1,26 @@
 from app.models.base_model import BaseModel
+from app import db, bcrypt
+import uuid
 
+# Association table between places and amenities (many-to-many)
+place_amenities = db.Table(
+    'place_amenities',
+    db.Column('place_id', db.Integer, db.ForeignKey('places.id'), primary_key=True),
+    db.Column('amenity_id', db.Integer, db.ForeignKey('amenities.id'), primary_key=True)
+)
 
 class Place(BaseModel):
+
+    __tablename__ = 'places'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     def __init__(self, title, description, price, latitude, longitude, owner):
         super().__init__()
@@ -15,12 +34,42 @@ class Place(BaseModel):
         self.reviews = []
     
     def add_amenity(self, amenity):
-        self.amenities.append(amenity)
-        self.save()
+        if amenity not in self.amenities:
+            self.amenities.append(amenity)
+            db.session.commit()
 
     def add_review(self, review):
         self.reviews.append(review)
-        self.save()
+        db.session.commit()
+     
+    def update_title(self, title):
+        self.title = title
+        db.session.commit()
+
+    def update_description(self, description):
+        self.description = description
+        db.session.commit()
+
+    def update_price(self, price):
+        self.price = price
+        db.session.commit()
+    
+    def update_location(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+        db.session.commit()
+
+    def delete_place(self):
+        """Delete the place and all associated reviews and amenities. To be updated as handled by cascade"""
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, data):
+        """Update the attributes of the place based on the provided dictionary."""
+        for key, value in data.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        db.session.commit()
 
     def __str__(self):
         """Return a string representation of the Place instance."""
@@ -29,48 +78,4 @@ class Place(BaseModel):
                 f"longitude={self.longitude}, owner={self.owner.id}, "
                 f"id={self.id}, created_at={self.created_at}, "
                 f"updated_at={self.updated_at})")
-    
-    def update_title(self, title):
-        self.title = title
-        self.save()
-
-    def update_description(self, description):
-        self.description = description
-        self.save()
-
-    def update_price(self, price):
-        self.price = price
-        self.save()
-    
-    def update_location(self, latitude, longitude):
-        self.latitude = latitude
-        self.longitude = longitude
-        self.save()
-
-    def delete_place(self):
-        """Delete the place and all associated reviews and amenities."""
-        for review in self.reviews:
-            review.delete_review()
-        self.reviews.clear()
-        
-        for amenity in self.amenities:
-            self.amenities.remove(amenity)
-        
-        self.title = None
-        self.description = None
-        self.price = None
-        self.latitude = None
-        self.longitude = None
-        self.owner = None
-        self.id = None
-        self.created_at = None
-        self.updated_at = None
-
-    def update(self, data):
-        """Update the attributes of the place based on the provided dictionary."""
-        for key, value in data.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
-        self.save()
-
     
