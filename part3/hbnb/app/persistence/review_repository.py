2d4f7fff556cbc1197e93e0c1420fb7d00171758
User10538 <10538@holbertonstudents.com>
2025-07-08 13:56:32 +1000

    Update review repo

--- part3/hbnb/app/persistence/review_repository.py
+++ part3/hbnb/app/persistence/review_repository.py
@@ -1,32 +1,37 @@
 from app.models.review import Review
+from app import db
 from app.persistence.repository import SQLAlchemyRepository
 
 class ReviewRepository(SQLAlchemyRepository):
-
     def __init__(self):
-        self._data = {}
+        super().__init__(Review)  # <-- pass the model to the base class
 
-    def get(self):
-        return self.review_repo.all()
-    
+    def get(self, review_id):
+        return Review.query.get(review_id)
+
     def add(self, review):
-        self._data[review.id] = review
+        db.session.add(review)
+        db.session.commit()
         return review
 
     def all(self):
-        return list(self._data.values())
+        return Review.query.all()
 
     def get_by_attribute(self, place_id):
-        return [review for review in self._data.values() if review.place_id == place_id]
-    
+        return Review.query.filter_by(place_id=place_id).all()
+
     def update(self, review_id, review_data):
-        review = self.get(review_id)
+        review = Review.query.get(review_id)
         if review:
             for key, value in review_data.items():
                 if hasattr(review, key):
                     setattr(review, key, value)
-            return review
-        return None
+            db.session.commit()
+        return review
 
     def delete(self, review_id):
-        return self._data.pop(review_id, None)
+        review = Review.query.get(review_id)
+        if review:
+            db.session.delete(review)
+            db.session.commit()
+        return review
