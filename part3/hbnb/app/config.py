a8396e2cc93fe93138c701b2b475ed862d2db9d2
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-07-08 19:01:09 +1000

    Entries can be added to db

--- part3/hbnb/app/config.py
+++ part3/hbnb/app/config.py
@@ -4,8 +4,10 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
 
-class DevelopmentConfig(Config):
+class DevelopmentConfig:
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:2Five2nine@127.0.0.1/hbnb_evo_2_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config = {
     'development': DevelopmentConfig,
--- part3/hbnb/app/persistence/__init__.py
+++ part3/hbnb/app/persistence/__init__.py
@@ -4,9 +4,9 @@ from sqlalchemy import create_engine, text, select
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 # Hardcoded credentials - PLEASE DON'T DO THIS IN PRODUCTION
-USER = "hbnb_admin1"
-PWD = "admin1_pwd"
-HOST = "localhost"
+USER = "root"
+PWD = "2Five2nine"
+HOST = "127.0.0.1"
 DB = "hbnb_evo_2_db"
 
 Base = declarative_base()
